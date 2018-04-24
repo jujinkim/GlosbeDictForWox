aa2b8c866d0485b3f0832655e216b47037682cf3
Jujin <jk012345@gmail.com>
2018-04-24 23:46:00 +0900

    Initialize

---
+++ GlosbeDictForWox/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wox.Plugin;
+
+namespace GlosbeDictForWox
+{
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context)
+        {
+
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+            
+        }
+    }
+}
